f99c3caf414a444f2cabbe0338dc9e25865a234c
Zahir Saiyed <zahir19saiyed@gmail.com>
2022-10-31 13:37:15 -0400

    Initial Commit

---
+++ HyprRun/HyprRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyprRunApp.swift
+//  HyprRun
+//
+//  Created by Zahir Saiyed on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct HyprRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
