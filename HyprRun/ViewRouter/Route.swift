1abc173e80612798998f4e31ad98e44b604baf16
Katie Lin <katielin019@gmail.com>
2022-11-11 13:03:35 -0500

    Working on refactoring view stuff

---
+++ HyprRun/ViewRouter/Route.swift
@@ -0,0 +1,15 @@
+//
+//  Route.swift
+//  HyprRun
+//
+//  Created by Katie Lin on 11/11/22.
+//
+
+import Foundation
+
+enum Route: String {
+  case splashView
+  case homeView
+  case runView
+  case postRunView
+}
