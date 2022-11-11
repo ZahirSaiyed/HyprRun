507cccde4bee6cd35192cace97777e57da6f781c
Katie Lin <katielin019@gmail.com>
2022-11-11 12:52:47 -0500

    Created CoreData class; will add core data model next

---
+++ HyprRun/Project Utilities/CoreDataStack.swift
@@ -0,0 +1,42 @@
+//
+//  CoreDataStack.swift
+//  HyprRun
+//
+//  Created by Katie Lin on 11/11/22.
+//  Reference: https://www.kodeco.com/553-how-to-make-an-app-like-runkeeper-part-1
+//
+
+import CoreData
+
+class CoreDataStack {
+  
+  static let persistentContainer: NSPersistentContainer = {
+    let container = NSPersistentContainer(name: "Runr3")
+    container.loadPersistentStores { (_, error) in
+      if let error = error as NSError? {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    }
+    
+    return container
+  }()
+  
+  static var context: NSManagedObjectContext {
+    return persistentContainer.viewContext
+  }
+  
+  class func saveContext() {
+    let context = persistentContainer.viewContext
+    
+    guard context.hasChanges else {
+      return
+    }
+    
+    do {
+      try context.save()
+    } catch {
+      let nserror = error as NSError
+      fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+    }
+  }
+}
