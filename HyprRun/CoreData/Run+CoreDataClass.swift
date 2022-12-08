443259711fea84772e1f96c2e1b58da35e97ee65
Katie Lin <katielin019@gmail.com>
2022-12-08 18:30:34 -0500

    Fixed file issue

--- HyprRun/CoreData/Run+CoreDataClass.swift
+++ HyprRun/CoreData/Run+CoreDataClass.swift
@@ -2,19 +2,14 @@
 //  Run+CoreDataClass.swift
 //  HyprRun
 //
-//  Created by Katie Lin on 12/8/22.
-//  References:
-//  - https://www.avanderlee.com/swift/nsmanagedobject-awakefrominsert-preparefordeletion/
+//  Created by Katie Lin on 12/6/22.
+//
 //
 
 import Foundation
 import CoreData
 
 
-public final class Run: NSManagedObject {
-  @NSManaged public var distance: Double
-  @NSManaged public var duration: Int16
-  @NSManaged public var timestamp: Date?
-  @NSManaged public var id: String
-  @NSManaged public var locations: NSOrderedSet?
+public class Run: NSManagedObject {
+
 }
